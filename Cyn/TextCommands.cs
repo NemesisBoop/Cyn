796bee6a81b771bea422a08693c5c27195122458
Neme <starrymere@gmail.com>
2025-06-09 18:36:46 -0500

    fixed doormat and added kick/ban commands

--- Cyn/TextCommands.cs
+++ Cyn/TextCommands.cs
@@ -15,28 +15,46 @@ namespace Cyn
 
         [Command("addrole")]
         [RequireUserPermission(GuildPermission.ManageRoles)]
-        public async Task AddRoleAsync(IGuildUser targetUser, [Remainder] string roleName)
+        public async Task AddRoleAsync([Remainder] string roleName, IGuildUser targetUser)
         {
             var guild = Context.Guild;
             var role = guild.Roles.FirstOrDefault(r => r.Name.Equals(roleName, StringComparison.OrdinalIgnoreCase));
 
             if (role == null)
             {
-                await ReplyAsync($"Role `{roleName}` not found.");
+                await ReplyAsync($"`{roleName}` doesn't exist.");
                 return;
             }
 
             await targetUser.AddRoleAsync(role);
             await ReplyAsync($"Added `{role.Name}` to {targetUser.Mention}");
         }
+        [Command("delrole")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        public async Task DelRoleAsync([Remainder] string roleName, IGuildUser targetUser)
+        {
+            var guild = Context.Guild;
+            var role = guild.Roles.FirstOrDefault(r => r.Name.Equals(roleName, StringComparison.OrdinalIgnoreCase));
+
+            if (role == null)
+            {
+                await ReplyAsync($"`{roleName}` doesn't exist.");
+                return;
+            }
+
+            await targetUser.RemoveRoleAsync(role);
+            await ReplyAsync($"Removed `{role.Name}` from {targetUser.Mention}.");
+        }
         [Command("doormat")]
         [RequireUserPermission(GuildPermission.ManageRoles)]
         public async Task DoormatAsync(IGuildUser targetUser)
         {
             var guild = Context.Guild;
-            var role = guild.Roles.FirstOrDefault(r => r.Name.Equals("\"Normal\" People", StringComparison.OrdinalIgnoreCase));
+            var AccessRole = guild.Roles.FirstOrDefault(r => r.Name.Equals("\"Normal\" People", StringComparison.OrdinalIgnoreCase));
+            var DeniedRole = guild.Roles.FirstOrDefault(r => r.Name.Equals("fed", StringComparison.OrdinalIgnoreCase));
 
-            await targetUser.RemoveRoleAsync(role);
+            await targetUser.RemoveRoleAsync(AccessRole);
+            await targetUser.AddRoleAsync(DeniedRole);
             await ReplyAsync($"Doormatted {targetUser.Mention}");
         }
         [Command("undoormat")]
@@ -44,10 +62,26 @@ namespace Cyn
         public async Task UndoormatAsync(IGuildUser targetUser)
         {
             var guild = Context.Guild;
-            var role = guild.Roles.FirstOrDefault(r => r.Name.Equals("\"Normal\" People", StringComparison.OrdinalIgnoreCase));
+            var AccessRole = guild.Roles.FirstOrDefault(r => r.Name.Equals("\"Normal\" People", StringComparison.OrdinalIgnoreCase));
+            var DeniedRole = guild.Roles.FirstOrDefault(r => r.Name.Equals("fed", StringComparison.OrdinalIgnoreCase));
 
-            await targetUser.AddRoleAsync(role);
-            await ReplyAsync($"Undoormatted {targetUser.Mention}");
+            await targetUser.RemoveRoleAsync(DeniedRole);
+            await targetUser.AddRoleAsync(AccessRole);
+            await ReplyAsync($"Unoormatted {targetUser.Mention}");
+        }
+        [Command("kick")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task KickAsync(IGuildUser targetUser)
+        {
+            await targetUser.KickAsync();
+            await ReplyAsync($"Kicked {targetUser.Mention}");
+        }
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task BanAsync(IGuildUser targetUser)
+        {
+            await targetUser.BanAsync();
+            await ReplyAsync($"Good riddance to {targetUser.Mention}");
         }
     }
 }
